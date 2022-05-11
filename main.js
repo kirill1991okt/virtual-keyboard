80964049b8f5369568f717f014097d7c766d8451
kirill1991okt <kirill1991okt@gmail.com>
2022-05-11 21:24:38 +0300

    feat: add prop for keys

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ class Keyboard {
 
     title.innerText = 'RSS Virtual keyboard';
     paragraph.innerText = 'Клавиатура была создана в Windows';
-    instruction.innerText = 'Для переключения ENG/РУС нажимай Ctrl + Alt';
+    instruction.innerText = 'Для переключения ENG/РУС нажимай Alt + Ctrl';
 
     textarea.autofocus = true;
     container.appendChild(creatKeyboardFragment);
@@ -47,6 +47,7 @@ class Keyboard {
 
     document.addEventListener('keydown', (e) =>{
       e.stopImmediatePropagation();
+      console.log(e);
       
       const key = document.querySelector(`#${e.code}`);
 
@@ -55,24 +56,87 @@ class Keyboard {
         return;
       }
 
+      
       if(e.key === 'CapsLock' && !e.repeat){
         e.preventDefault();
-        this.caps = true;
+        this.caps = !this.caps;
 
+        if(this.caps){
+          key.classList.add('active');
+        } else{
+          key.classList.remove('active');
+        }
+        this.switchCaps(e.shiftKey);
+      } else {
         key.classList.add('active');
 
-        this.switchCaps(e.shiftKey); // поставить аргумент в функцию.
+        if(e.altKey && e.ctrlKey && !e.repeat){
+          this.lang = this.lang === 'ru' ? 'en' : 'ru';
+          localStorage.setItem('lang', this.lang);
+          this.switchLang(this.lang, e.shiftKey);
+        } else if (e.key === 'Shift' && !e.repeat){
+          e.preventDefault();
+          this.switchCaps(e.shiftKey);
+        } else if (e.code === 'Tab' && !e.repet){
+          e.preventDefault();
+          this.putText('\t');
+        } else if (e.code === 'Enter' && !e.repet){
+          e.preventDefault();
+          this.putText('\n');
+        } else if (e.code === 'Backspace' && !e.repet){
+          e.preventDefault();
+          this.pressBackspace();
+        } else if (e.code === 'Delete' && !e.repet){
+          e.preventDefault();
+          this.pressDelete();
+        } else if (e.code === 'ArrowUp' && !e.repet){
+          e.preventDefault();
+          this.arrowUp();
+        } else if (e.code === 'ArrowDown' && !e.repet){
+          e.preventDefault();
+          this.arrowDown();
+        } else if (e.code === 'ArrowLeft' && !e.repet){
+          e.preventDefault();
+          this.arrowLeft();
+        } else if (e.code === 'ArrowRight' && !e.repet){
+          e.preventDefault();
+          this.arrowRight();
+        } else if (!objectWithKeyboardKyes[e.code].func){
+          e.preventDefault();
+          this.putText(key.textContent);
+        } 
+
       }
-      
+    })
+
+    document.addEventListener('keyup', (e) => {
+      e.stopImmediatePropagation();
+      const key = document.querySelector(`#${e.code}`);
+
+      if(!key){
+        e.preventDefault();
+        return;
+      }
+
+      if(e.key !== 'CapsLock'){
+        key.classList.remove('active');
+
+        
+        if(e.key === 'Shift'){
+          console.log(this.caps);
+          console.log(e);
+          e.preventDefault();
+          this.switchCaps(e.shiftKey);
+        }
+      }
     })
   }
+
               //false
   switchCaps(shiftKey){
     const showUpperCase = (this.caps && !shiftKey) || (!this.caps && shiftKey);
-
     
     this.container.querySelectorAll('.keyboard-key').forEach(e => {
-      // console.log(objectWithKeyboardKyes[e.id].func)
       if (!objectWithKeyboardKyes[e.id].func) {
         if (e.id === 'Backquote' && this.lang === 'en') {
           e.textContent = shiftKey ? '~' : '`';
@@ -108,10 +172,76 @@ class Keyboard {
       } 
     });
 
-    
+  }
+
+ // потестировать это функцию
+switchLang(lang, shift){
+  this.container.querySelectorAll('.keyboard-key').forEach(e =>{
+    e.textContent = objectWithKeyboardKyes[e.id][lang];
+  })
+
+  this.switchCaps(shift);
+}
+
+putText(char){
+  let text = this.text,
+  start = text.selectionStart,
+  end = text.selectionEnd;
+
+  text.value = text.value.slice(0, start) + char + text.value.slice(end);
+
+  start = start + char.length;
+  end = start; 
+}
+
+pressBackspace(){
+  if (this.text.selectionStart !== this.text.selectionEnd) {
+    this.putText('');
+  } else {
+    this.text.value = this.text.value.slice(0, this.text.selectionStart - 1) + this.text.value.slice(this.text.selectionEnd);
+
+    this.text.selectionStart = this.text.selectionStart;
+    this.text.selectionEnd = this.text.selectionStart;
+
   }
 }
 
+pressDelete(){
+  if (this.text.selectionStart !== this.text.selectionEnd) {
+    this.putText('');
+  } else {
+    const currentPosition = this.text.selectionStart;
+    this.text.value = this.text.value.slice(0, this.text.selectionStart) + this.text.value.slice(this.text.selectionEnd + 1);
+
+    this.text.selectionStart = currentPosition;
+    this.text.selectionEnd = this.text.selectionStart;
+  }
+}
+
+arrowUp(){
+  this.text.selectionStart = 0;
+  this.text.selectionEnd = this.text.selectionStart;
+  console.log('up');
+}
+
+arrowDown(){
+  this.text.selectionStart = this.text.value.length;
+  this.text.selectionEnd = this.text.selectionStart;
+  console.log('down');
+}
+
+arrowLeft(){
+  this.text.selectionStart = this.text.selectionStart - 1;
+  this.text.selectionEnd = this.text.selectionStart;
+  console.log('left');
+}
+
+arrowRight(){
+  this.text.selectionStart = this.text.selectionStart + 1;
+  this.text.selectionEnd = this.text.selectionStart;
+  console.log('right');
+}
+
+}
+
 new Keyboard().init();
-// const keyboard = new Keyboard();
-// console.log(keyboard);
